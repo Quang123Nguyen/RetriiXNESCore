d8ac431d460f0432e9ab3b59130fc5e0a24d41af
Quang123Nguyen <quang.github.nguyen@gmail.com>
2025-10-08 10:23:10 +0700

    Init again

---
+++ Nestopia/Nestopia.swift
@@ -0,0 +1,10 @@
+//
+//  Nestopia.swift
+//  Nestopia
+//
+//  Created by Vũ Thị Thanh on 9/2/25.
+//
+
+class Nestopia {
+
+}
---
+++ NestopiaJS/NESEmulatorBridge.hpp
@@ -0,0 +1,51 @@
+//
+//  NESEmulatorBridge.hpp
+//  NESDeltaCore
+//
+//  Created by  on 6/1/18.
+//  Copyright © 2018 . All rights reserved.
+//
+
+#ifndef NESEmulatorBridge_hpp
+#define NESEmulatorBridge_hpp
+
+#include <stdio.h>
+
+#if defined(__cplusplus)
+extern "C"
+{
+#endif
+    typedef void (*BufferCallback)(const unsigned char *_Nonnull buffer, int size);
+    typedef void (*VoidCallback)(void);
+    
+    double NESFrameDuration();
+    
+    void NESInitialize(const char *_Nonnull databasePath);
+    
+    bool NESStartEmulation(const char *_Nonnull gamePath);
+    void NESStopEmulation();
+    
+    void NESRunFrame();
+    
+    void NESActivateInput(int input, int playerIndex);
+    void NESDeactivateInput(int input, int playerIndex);
+    void NESResetInputs();
+    
+    void NESSaveSaveState(const char *_Nonnull saveStatePath);
+    void NESLoadSaveState(const char *_Nonnull saveStatePath);
+    
+    void NESSaveGameSave(const char *_Nonnull gameSavePath);
+    void NESLoadGameSave(const char *_Nonnull gameSavePath);
+    
+    bool NESAddCheatCode(const char *_Nonnull cheatCode);
+    void NESResetCheats();
+    
+    void NESSetAudioCallback(_Nullable BufferCallback audioCallback);
+    void NESSetVideoCallback(_Nullable BufferCallback videoCallback);
+    void NESSetSaveCallback(_Nullable VoidCallback saveCallback);
+    
+#if defined(__cplusplus)
+}
+#endif
+
+#endif /* NESEmulatorBridge_hpp */
